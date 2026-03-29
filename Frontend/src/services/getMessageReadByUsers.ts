2d70a44f3ae0deee7078479585e34c3a298316d7
Jason Mac <jasonvanmac@gmail.com>
2026-03-28 18:54:14 -0700

    [debug] fix seen message on real time

--- Frontend/src/services/getMessageReadByUsers.ts
+++ Frontend/src/services/getMessageReadByUsers.ts
@@ -1,12 +1,9 @@
 import { API_URL } from '../config';
-import type { MessageReadEntries } from '../types/messageRead';
-
-//.route("/messages/{id}/read", get(get_message_read))
-//.route("/messages/{id}/read", post(mark_message_read));
+import type { MessageReadsResponse } from '../types/messageRead';
 
 export const fetchMessageReadEntries = async (
   message_id: string
-): Promise<MessageReadEntries> => {
+): Promise<MessageReadsResponse> => {
   const res = await fetch(`${API_URL}/messages/${message_id}/read`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}`,
